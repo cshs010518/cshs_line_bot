bbd5c693bad72e4d57078c1144d0334ba727623a
cshs010518 <150873023+cshs010518@users.noreply.github.com>
2023-11-29 10:02:45 +0800

    Update s010518.py

--- student_modules/s010518.py
+++ student_modules/s010518.py
@@ -1,2 +1,7 @@
 def process(message: str) -> None:
-    pass
+    if message =="12月合作角":
+        return "希望能讓我滿意"
+    if message =="為美好世界獻上祝福":
+        return "我不需要祝福"
+    if message =="手機消失":
+        return "尋找手機"
